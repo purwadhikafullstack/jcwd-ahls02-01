28ad41665c662464c18fe33142fe027c308e9f79
=Jackfrost24a <=randystvn@gmail.com>
2022-09-06 21:42:37 +0700

    Created stock convertion

--- projects/client/src/Redux/Actions/productsAction.js
+++ projects/client/src/Redux/Actions/productsAction.js
@@ -1,7 +1,19 @@
-export const getProductsAction = (data) => {
-    console.log("DATA DARI COMPONENT UI", data)
-    return {
-        type: "GET_PRODUCTS",
-        payload: data
-    }
+import axios from "axios"
+import { API_URL } from "../../helper"
+
+
+export const getProductActions = (data) => {
+  console.log("data PRODUCT dari component UI", data)
+  return {
+    type: "GET_PRODUCT",
+    payload: data
+  }
+}
+
+export const getAllProductActions = (data) => {
+  console.log("data ALL PRODUCT dari component UI", data)
+  return {
+    type: "GET_ALLPRODUCT",
+    payload: data
+  }
 }
