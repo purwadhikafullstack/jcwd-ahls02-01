f59de1232d81b898265d6afe3d122c3f0e410cbd
Abdi Alghifara Felinanda <52093497+Abdi-01@users.noreply.github.com>
2022-09-16 14:05:22 +0700

    Update helper.js
    Change helper

--- projects/client/src/helper.js
+++ projects/client/src/helper.js
@@ -1,5 +1,5 @@
-export const API_URL = "http://localhost:8000/api";
+export const API_URL = "https://jcwd-ahls02-01.purwadhikabootcamp.com/api";
 
-export const FE_URL = "http://localhost:3000";
+export const FE_URL = "https://jcwd-ahls02-01.purwadhikabootcamp.com";
 
-export const BE_URL = "http://localhost:8000";
+export const BE_URL = "https://jcwd-ahls02-01.purwadhikabootcamp.com";
