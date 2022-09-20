29711c1a266e4339c601dcae36bc2449887b211d
dimasadityam <dimas11adityam@gmail.com>
2022-09-20 17:49:39 +0700

    developmet1-4 : dbConnection error

--- projects/server/src/Config/database.js
+++ projects/server/src/Config/database.js
@@ -14,7 +14,7 @@ const dbConf = mysql.createPool({
   timeout: 60 * 60 * 1000,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: "3306",
 });
