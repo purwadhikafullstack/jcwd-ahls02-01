27b22207e28ffc1536835f532392fd09128537ca
dimasadityam <dimas11adityam@gmail.com>
2022-09-22 08:20:15 +0700

    resolve bug profile picture crop, hover, default image, progress password

--- projects/client/src/helper.js
+++ projects/client/src/helper.js
@@ -3,3 +3,5 @@ export const API_URL = "https://jcwd-ahls02-01.purwadhikabootcamp.com/api";
 export const FE_URL = "https://jcwd-ahls02-01.purwadhikabootcamp.com";
 
 export const BE_URL = "https://jcwd-ahls02-01.purwadhikabootcamp.com";
+// export const API_URL = "http://localhost:8000/api";
+// export const BE_URL = "http://localhost:8000";
